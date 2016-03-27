19b8cc82d8b552318a4276d8b826a1a095593f55
antiproton <dr.urban@netreal.de>
2016-03-27 09:13:35 +0200

    test

--- test.php
+++ test.php
@@ -6,94 +6,26 @@
 <html>
  <head>
   <title>Ajax Beispiel 002</title>
-  <script type="text/javascript">
-  <!--
-	var request = false;
-
-	// Request senden
-	function setRequest(value) {
-		// Request erzeugen
-		if (window.XMLHttpRequest) {
-			request = new XMLHttpRequest(); // Mozilla, Safari, Opera
-		} else if (window.ActiveXObject) {
-			try {
-				request = new ActiveXObject('Msxml2.XMLHTTP'); // IE 5
-			} catch (e) {
-				try {
-					request = new ActiveXObject('Microsoft.XMLHTTP'); // IE 6
-				} catch (e) {}
-			}
-		}
-
-		// überprüfen, ob Request erzeugt wurde
-		if (!request) {
-			alert("Kann keine XMLHTTP-Instanz erzeugen");
-			return false;
-		} else {
-			var url = "ajax.php";
-			// Request öffnen
-			request.open('post', url, true);
-			// Requestheader senden
-			request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-			// Request senden
-			request.send('name='+value);
-			// Request auswerten
-			request.onreadystatechange = interpretRequest;
-		}
-	}
-
-	// Request auswerten
-	function interpretRequest() {
-		switch (request.readyState) {
-			// wenn der readyState 4 und der request.status 200 ist, dann ist alles korrekt gelaufen
-			case 4:
-				if (request.status != 200) {
-					alert("Der Request wurde abgeschlossen, ist aber nicht OK\nFehler:"+request.status);
-				} else {
-					 content = request.responseText;
-
-                              mydaten(content);
-
-
-				}
-				break;
-			default:
-				break;
-		}
-	}
-
-
-  //-->
-  </script>
+<script type="text/javascript" src="js/ajax.js"></script>
 
 
   <script type="text/javascript">
+setInterval(function(){ setRequest();
 
-
-
-
-
-  setInterval(function(){ setRequest();
-
-
-
-  var ts = Math.round((new Date()).getTime() / 1000); //timestamp
+ var ts = Math.round((new Date()).getTime() / 1000); //timestamp
                      document.getElementById('ts').innerHTML = ts;
-
 
   }, 1000);
 
 
 function mydaten(content) {
-   
+
 
   // den Inhalt des Requests in das <div> schreiben
-                    document.getElementById('content').innerHTML = content;  
-   
-    return;              
+                    document.getElementById('content').innerHTML = content;
+
+    return;
 }
-
-
 
    </script>
 
